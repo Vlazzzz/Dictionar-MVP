4156d2ff34694ecc44e8e1960d8541c2cb4a4b78
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2024-03-13 14:18:44 +0200

    update menu item

--- tema1_dictionar/tema1_dictionar/MainWindow.xaml.cs
+++ tema1_dictionar/tema1_dictionar/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ namespace tema1_dictionar
     {
 
         private List<Word> words;
+        private bool searchByWord = true;
+
 
         public MainWindow()
         {
@@ -41,60 +43,133 @@ namespace tema1_dictionar
         private void ComboBox_GotFocus(object sender, RoutedEventArgs e)
         {
             ComboBox comboBox = (ComboBox)sender;
-            if (comboBox.Text == "Cuvânt")
+            if (comboBox.Text == "Cuvânt" || comboBox.Text == "Categorie")
             {
                 comboBox.Text = string.Empty;
                 comboBox.Foreground = Brushes.Black; // Change the text color to black
+
+                // Populate the drop-down list with categories if searching by category
+                if (!searchByWord)
+                {
+                    var categories = words.Select(w => w.Category).Distinct();
+                    myComboBox.ItemsSource = categories;
+                }
+                else
+                {
+                    // Clear the auto-complete suggestions
+                    myComboBox.ItemsSource = null;
+                }
             }
         }
 
-        private void ComboBox_KeyDown(object sender, KeyEventArgs e)
+
+        private void ComboBox_KeyDown_searchWords(object sender, KeyEventArgs e)
         {
-            myComboBox.IsDropDownOpen = true;
-
-            ComboBox comboBox = (ComboBox)sender;
-            string searchText = comboBox.Text.ToLower(); // Retrieve text from the ComboBox
-
-            // If the search text is empty, do not perform search
-            if (string.IsNullOrWhiteSpace(searchText))
+            if (searchByWord)
             {
-                // Clear the auto-complete suggestions
-                myComboBox.ItemsSource = null;
-                return;
-            }
-            // Filter words that start with the search text
-            var matchedWords = words.Where(w => w.Text.ToLower().StartsWith(searchText)).Select(w => w.Text);
-            // Display matched words as auto-complete options
-            myComboBox.ItemsSource = matchedWords;
-        }
+                myComboBox.IsDropDownOpen = true;
 
-        private void SearchButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Retrieve the text from the ComboBox
-            string searchText = myComboBox.Text.Trim();
+                ComboBox comboBox = (ComboBox)sender;
+                string searchText = comboBox.Text.ToLower(); // Retrieve text from the ComboBox
 
-            // Find the word in the list of words
-            Word selectedWord = words.FirstOrDefault(w => w.Text.Equals(searchText, StringComparison.OrdinalIgnoreCase));
+                // If the search text is empty, do not perform search
+                if (string.IsNullOrWhiteSpace(searchText))
+                {
+                    // Clear the auto-complete suggestions
+                    myComboBox.ItemsSource = null;
+                    return;
+                }
 
-            if (selectedWord != null)
-            {
-                // Display the word
-                wordNameBlock.Text = $"{selectedWord.Text}";
-
-                // Display the category
-                wordDescriptionBlock.Text = $"Categorie: {selectedWord.Category}";
-
-                // Append the description to the wordTextBlock
-                wordDescriptionBlock.Text += $" - {selectedWord.Description}";
-
-                // Display the image
-                wordImageBlock.Source = new BitmapImage(new Uri(selectedWord.ImagePath));
+                // Filter words that start with the search text
+                var matchedWords = words.Where(w => w.Text.ToLower().StartsWith(searchText)).Select(w => w.Text);
+                // Display matched words as auto-complete options
+                myComboBox.ItemsSource = matchedWords;
             }
             else
             {
-                // Word not found, display a message or handle the case accordingly
-                MessageBox.Show("Word not found.");
+                myComboBox.IsDropDownOpen = true;
+                //Retrieve all the categories from the list of words
+                var categories = words.Select(w => w.Category).Distinct();
+                myComboBox.ItemsSource = categories;
             }
+        }
+
+        //eu vreau ca atunci cand selectez butonul de categorie, auto_completul sa imi arate toate cuvintele din categoria respectiva
+        //dar daca selectez butonul de cuvant, sa imi arate toate cuvintele care se potrivesc cu ce am scris in casuta de cautare
+
+        //totodata, daca butonul de cautare este pe categorie, vreau ca autocomplete-ul sa mi arata categoriile ce dau match cu ce am scris eu
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (searchByWord)
+            {
+                // Retrieve the text from the ComboBox
+                string searchText = myComboBox.Text.Trim();
+
+                // Find the word in the list of words
+                Word selectedWord = words.FirstOrDefault(w => w.Text.Equals(searchText, StringComparison.OrdinalIgnoreCase));
+
+                if (selectedWord != null)
+                {
+                    // Display the word
+                    wordNameBlock.Text = $"{selectedWord.Text}";
+
+                    // Display the category
+                    wordDescriptionBlock.Text = $"Categorie: {selectedWord.Category}";
+
+                    // Append the description to the wordTextBlock
+                    wordDescriptionBlock.Text += $" - {selectedWord.Description}";
+
+                    // Display the image
+                    wordImageBlock.Source = new BitmapImage(new Uri(selectedWord.ImagePath));
+                }
+                else
+                {
+                    // Word not found, display a message or handle the case accordingly
+                    MessageBox.Show("Word not found.");
+                }
+            }
+            else
+            {
+                // Retrieve the selected category from the ComboBox
+                string selectedCategory = myComboBox.Text.Trim();
+
+                // Filter the list of words based on the selected category
+                var wordsInCategory = words.Where(w => w.Category.Equals(selectedCategory, StringComparison.OrdinalIgnoreCase));
+
+                if (wordsInCategory.Any())
+                {
+                    // Populate the ComboBox with words from the selected category
+                    myComboBox.ItemsSource = wordsInCategory.Select(w => w.Text);
+                    searchByWord = true;
+                    searchFilter.Content = "Categorie";
+                }
+                else
+                {
+                    MessageBox.Show("No words found in this category.");
+                }
+            }
+        }
+
+        private void AddWord_Click(object sender, RoutedEventArgs e)
+        {
+            UserMainWindow userMainWindow = new UserMainWindow();
+            userMainWindow.Show(); 
+            this.Close();
+        }
+
+        private void ModifyWord_Click(object sender, RoutedEventArgs e)
+        {
+            UserMainWindow userMainWindow = new UserMainWindow();
+            userMainWindow.Show();
+            this.Close();
+        }
+
+        private void RemoveWord_Click(object sender, RoutedEventArgs e)
+        {
+            UserMainWindow userMainWindow = new UserMainWindow();
+            userMainWindow.Show();
+            this.Close();
         }
 
         private void ImageButton_MouseDown(object sender, MouseButtonEventArgs e)
@@ -102,5 +177,30 @@ namespace tema1_dictionar
             // Handle the button click event here
         }
 
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (searchByWord == true)
+            {
+                myComboBox.Text = "Categorie";
+                searchByWord = false;
+                searchFilter.Content = "Cuvânt";
+            }
+            else
+            {
+                myComboBox.Text = "Cuvânt";
+                searchByWord = true;
+                searchFilter.Content = "Categorie";
+            }
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
